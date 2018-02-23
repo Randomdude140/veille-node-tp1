485f8849138530f2c35000c7c28d5d52268cb310
Arnaud Voyer <ArnaudRandomdudeVoyer@gmail.com>
2018-02-23 14:20:23 -0500

    Ajout de tableaux nom et prénom

---
+++ mes_modules/tableaux.js
@@ -0,0 +1,24 @@
+"use strict";
+const tabNom [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
+]
+
+const tabPrenom [
+    "Caitlin",
+    "Kaytelan",
+    "Catlin",
+    "Katelyn",
+    "Cayetlyn",
+    "Caitlinn",
+    "Kateland",
+    "Kaytelynn",
+    "Catlyn"
+]
